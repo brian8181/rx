fc1b9d847509b09144e448c152ddbb405d50aa9b
Brian Preston <brian8181@gmail.com>
2020-07-09 12:43:03 -0500

    autotool experiments

---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "rx"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "brian8181@gmail.com.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "rx"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "rx 0.01"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "rx"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://brian8181.com"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.01"
+
+/* Version number of package */
+#define VERSION "0.01"

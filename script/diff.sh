e5da14856daebf722dd7e621ba11a584ce6428c4
Brian K <brian8181@gmail.com>
2021-03-27 21:25:12 -0500

    getting auto stuff to work

--- script/diff.sh
+++ script/diff.sh
@@ -6,13 +6,4 @@ RIDX=$3
 #EXEC=~/script/git/generic_diff.sh
 EXEC_PARAMS="diff -y"
 
-#echo "Debug: ROOT:$ROOT NAME:${FULL_NAME##/*/} FULL_NAME:$FULL_NAME LIDX:$LIDX RIDX:$RIDX"
-
-#LHASH=$(~/script/git_hist.sh $FULL_NAME | egrep "^\s{4,5}$LIDX\s" | cut -d'|' -f2)
-#RHASH=$(~/script/git_hist.sh $FULL_NAME | egrep "^\s{4,5}$RIDX\s" | cut -d'|' -f2)
-
-#echo "Debug: EXEC:$EXEC LHASH:$LHASH RHASH:$RHASH"
-
-#$EXEC $FULL_NAME $LHASH $RHASH "$EXEC_PARAMS"
-
 ./generic_diff.sh $NAME $LIDX $RIDX "$EXEC_PARAMS"

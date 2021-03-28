930f6ca8ddd868a048470ac69647439f5b182eff
Brian K <brian8181@gmail.com>
2021-03-28 18:53:35 -0500

    creating template file

---
+++ script/template.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+FILE='<FILE NAME>'
+VERSION='<VERSION.SUBVERSION>'
+FILE_DATE='<DATE>'
+
+AUTHOR='Brian Preston'
+EMAIL='brian8181@gmail.com'
+GITHUB='https://github.com/brian8181'
+
+VERBOSE=1
+DEBUG=1
+FMT_FG_RED='\e[31m'
+FMT_RESET='\e[0m'
+
+echo ${VERBOSE:+"File - $FILE"}
+echo ${VERBOSE:+"Version - $VERSION"}
+echo ${VERBOSE:+"Date - $FILE_DATE"}
+echo ${VERBOSE:+"Author - $AUTHOR"}
+echo ${VERBOSE:+"Email - $EMAIL"}
+echo ${VERBOSE:+"GITHUB - $GITHUB"}
+
+DATE=$(date "+%H:%M:%S:%s")
+
+echo -e "${DEBUG:+${FMT_FG_RED}DEBUG${FMT_RESET} - $FILE -> Running... @ $DATE}"
+
+# YOUR CODE GOES HERE
+
+echo -e "${DEBUG:+${FMT_FG_RED}DEBUG${FMT_RESET} - $FILE -> Exiting. @ $DATE}"

11a6fddf7fc40d0251ba0a5184816783dca45abb
Brian K <brian8181@gmail.com>
2021-03-28 18:43:12 -0500

    creating a template file

--- script/generic_diff.sh
+++ script/generic_diff.sh
@@ -1,19 +1,32 @@
-#! /usr/bin/bash
+#!/usr/bin/bash
+
+FILE='git_generic.sh'
+VERSION='0.1'
+DATE='03-28-2012'
+
+AUTHOR='Brian Preston'
+EMAIL='brian8181@gmail.com'
+GITHUB='https://github.com/brian8181'
+
+VERBOSE=$5
+#DEBUG=1
+
+echo ${VERBOSE:+"File - $FILE"}
+echo ${VERBOSE:+"Version - $VERSION"}
+echo ${VERBOSE:+"Date - $DATE"}
+echo ${VERBOSE:+"Author - $AUTHOR"}
+echo ${VERBOSE:+"Email - $EMAIL"}
+echo ${VERBOSE:+"GITHUB - $GITHUB"}
 
 ROOT=/home/brian/src/rx
 FULL_NAME=$ROOT/src/$1
 NAME=src/$1
 LIDX=$2
 RIDX=$3
-EXEC=~/script/git/git_diff.sh
 EXEC_PARAMS="$4"
-
-echo "Debug: ROOT:$ROOT NAME:${FULL_NAME##/*/} FULL_NAME:$FULL_NAME LIDX:$LIDX RIDX:$RIDX"
 
 LHASH=$(~/script/git/git_hist.sh $FULL_NAME | egrep "^\s{4,5}$LIDX\s" | cut -d'|' -f'2 13' | sed s/\|/~/)
 RHASH=$(~/script/git/git_hist.sh $FULL_NAME | egrep "^\s{4,5}$RIDX\s" | cut -d'|' -f'2 13' | sed s/\|/~/)
 
-
-echo "Debug: EXEC:"$EXEC" $NAME-LHASH:$LHASH $NAME-RHASH:$RHASH"
-
-$EXEC $FULL_NAME $LHASH $RHASH "$EXEC_PARAMS"
+EXEC="/home/brian/src/script/git/git_diff.sh"
+$EXEC "$FULL_NAME" "$LHASH" "$RHASH" "$EXEC_PARAMS"
---
+++ script/kdiff3.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/bash
+
+ROOT=/home/brian/src/rx
+FULL_NAME=$ROOT/$NAME
+NAME=$1
+LIDX=$2
+RIDX=$3
+
+# lanch diff in meld
+EXEC_PARAMS="kdiff3"
+
+./generic_diff.sh $NAME $LIDX $RIDX "$EXEC_PARAMS"

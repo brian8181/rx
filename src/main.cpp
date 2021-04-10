af24c10c4376397cde07c1e45d83ee483eec8fd8
brian <brian8181@gmail>
2021-04-10 14:21:34 -0500

    added build dir

--- src/main.cpp
+++ src/main.cpp
@@ -14,15 +14,16 @@ main(int argc, char* argv[])
         // get input from pipe
         char buffer[BUFFER_LEN]; // buffer for pipe  
         cin >> buffer;
-
+		
+			
         // add piped buffer to end of args
         // note: create array of char*, number of char* is argc
         // char* argv_tmp[] = new char* [argc];
-        char* argv_tmp[sizeof(char*) * argc+1];
-        memcpy(argv_tmp, argv, sizeof(char*) * argc);
-        argv_tmp[argc] = buffer;
+        char* argvtmp[sizeof(char*) * argc+1];
+        memcpy(argvtmp, argv, sizeof(char*) * argc);
+        argvtmp[argc] = buffer;
 
-        argv = argv_tmp;
+        argv = argvtmp;
         return parse_options(++argc, argv);
     }
     return parse_options(argc, argv);

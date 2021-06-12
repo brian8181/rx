dd034f8f8a91a24cc75afa3cc99ae90818742538
Brian Preston <brian8181@gmail.com>
2021-06-12 18:19:10 -0500

    create bash_color_test to test bash_colors.h

---
+++ src/bash_color_test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "bash_color.h"
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    cout << FMT_FG_RED << "Hello World!" << FMT_RESET << endl;
+    cout << FMT_FG_BLUE << "Hello World!"  << FMT_RESET << endl;
+    cout << FMT_FG_GREEN << "Hello World!"  << FMT_RESET << endl;
+
+    return 0;
+}

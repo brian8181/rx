4261d2104704b2b2548fc85cc7bef07767e819b0
Brian Preston <brian8181@gmail.com>
2021-08-07 15:53:04 -0500

    remove remove class files

--- cleandist.sh
+++ cleandist.sh
@@ -1,3 +1,4 @@
 rm configure Makefile libtool aclocal.m4 compile INSTALL install-sh ltmain.sh config.* Makefile.in src/Makefile.in depcomp stamp-h1
 rm -r  autom4te.cache
+rm null.d
 #rm flags.hpp main.o Makefile Makefile.in rx rx.o

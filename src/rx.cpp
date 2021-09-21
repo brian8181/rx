3f04fad3f5f4e5e9aa6ec4dc77bfd28f7271b6ce
Brian Preston <brian8181@gmail.com>
2021-09-20 21:51:11 -0500

    change output (match numbering)

--- src/rx.cpp
+++ src/rx.cpp
@@ -87,7 +87,7 @@ int regx_match(int count, char* args[])
 				// reset bash color position
 				pos += CURRENT_FG_COLOR.length() + len;
 				bash_stdio.insert(pos, FMT_RESET);
-				cout << (match_i+1) << ": " << src.substr(match.position(), match.length());
+				cout << (match_i+1) << ": ";// << src.substr(match.position(), match.length());
 			}
 			else
 			{

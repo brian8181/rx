c937e30e36193275c4f61e2d4e6d8e1125106c32
Brian Preston <brian8181@gmail.com>
2021-10-07 05:16:49 -0500

    update Makefile, continue update rx.cpp for regex options option "-o"

--- src/rx.hpp
+++ src/rx.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <map>
 #include <vector>
-//#include <regex>
+#include <regex>
 
 
 using std::map;
@@ -41,17 +41,49 @@ const unsigned int gmask      = 0x0000003F;
 const unsigned int icase      = 0x00000100;
 const unsigned int nosubs     = 0x00000200;
 const unsigned int optimize   = 0x00000400;
-const unsigned int collate_   = 0x00000800;
+const unsigned int collate  = 0x00000800;
+
+// const unsigned int RXOPT_ECMAScript = 0x00000001;
+// const unsigned int RXOPT_basic      = 0x00000002;
+// const unsigned int RXOPT_extended   = 0x00000004;
+// const unsigned int RXOPT_awk        = 0x00000008;
+// const unsigned int RXOPT_grep       = 0x00000010;
+// const unsigned int RXOPT_egrep      = 0x00000020;
+// const unsigned int RXOPT_gmask      = 0x0000003F;
+// const unsigned int RXOPT_icase      = 0x00000100;
+// const unsigned int RXOPT_nosubs     = 0x00000200;
+// const unsigned int RXOPT_optimize   = 0x00000400;
+// const unsigned int RXOPT_collate   = 0x00000800;
+
 unsigned int REGX_FLAGS = 0;
 unsigned char OPTION_FLAGS = DEFAULTS;
 
-std::map<std::string, unsigned int> enum_map = {
+//regex::flag_type t;
+
+// std::map<std::string, regex::flag_type> enum_map = {
+//     {"ECMAScript", regex::ECMAScript}, 
+//     // {"basic ", regex::basic },
+//     // {"extended", regex::extended},
+//     // {"awk", regex::awk},
+//     // {"grep", regex::grep}, 
+//     // {"egrep", regex::egrep},
+//     // {"icase", regex::icase},
+//     // {"nosubs", regex::nosubs},
+//     // {"optimize", regex::optimize},
+//     // {"_collate", regex::collate_}
+// };
+
+std::map<std::string, int> enum_map = {
     {"ECMAScript", ECMAScript}, 
     {"basic ", basic },
     {"extended", extended},
     {"awk", awk},
-    {"grep", grep},
-    {"icase", icase}
+    {"grep", grep}, 
+    {"egrep", egrep},
+    {"icase", icase},
+    {"nosubs", nosubs},
+    {"optimize", optimize},
+    {"collate", collate}
 };
 
 

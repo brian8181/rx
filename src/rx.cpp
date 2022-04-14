a8ae39c072745a0138b829719cf4887b9f1d9781
brian <brian8181@gmail.com>
2022-04-13 19:30:52 -0500

    add fpot

--- src/rx.cpp
+++ src/rx.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include <string>
 #include <regex>
 #include <vector>
@@ -204,12 +205,21 @@ int parse_options(int argc, char* argv[])
 			return 0;
 		case 'f':
 		{
-			if(argc != optind)
-				break;
+			// if(argc != optind)
+			// {
+			// 	string tmp_opt;
+			// 	cin >> tmp_opt;
+			// 	if(tmp_opt == "-f")
+			// 	{
+
+			// 	}
+			// 	break;
+			// }
 			
 			OPTION_FLAGS |= FROM_FILE;
 			ifstream search_file;
 			search_file.open(optarg, ios::in); 
+			
 			
 			if (search_file.is_open())
 			{   
@@ -272,8 +282,8 @@ int parse_options(int argc, char* argv[])
 	
 	if((OPTION_FLAGS & FROM_FILE) == 0)
 	{
-		// cout << "argc: " << argc << endl;
-		// cout << "optind: " << optind << endl;
+		cout << "argc: " << argc << endl;
+		cout << "optind: " << optind << endl;
 
 		int idx = optind + 1;
 		search_text.assign(argv+idx, (argv+idx) + (argc-idx));

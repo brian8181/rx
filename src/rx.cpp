dd0740d5013c7ef971802a04abcf3fac2dc50e28
Brian Preston <brian8181@gmail.com>
2022-06-14 17:06:19 -0500

    checking in all

--- src/rx.cpp
+++ src/rx.cpp
@@ -271,12 +271,11 @@ int parse_options(int argc, char* argv[])
 		cerr << "Expected argument after options, -h for help" << endl;
 		return -1;
 	}
-		
+
+	exp_text.push_back(argv[optind]);			
 	if((OPTION_FLAGS & FROM_FILE) == 0)
 	{
 		search_text.assign(argv+(optind+1), argv + argc);
 	}
-
-	exp_text.push_back(argv[optind]);	
 	return regx_match(exp_text, search_text);
 }

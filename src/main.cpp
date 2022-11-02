ffb25ad4952294f5f3bded498715f5be5457030e
Brian K Preston <brian@fire.tx.rr.com>
2022-11-02 07:46:48 -0500

    moved Makefile one to root, one to doc, clean up Makefile, empty build directory

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ int main(int argc, char* argv[])
 			char* argvtmp[sizeof(char*) * argc+1];
 			memcpy(argvtmp, argv, sizeof(char*) * argc);
 			argvtmp[argc] = &buffer[0];
-			//argv = argvtmp;
+			argv = argvtmp;
 			++argc;
 			return parse_options(argc, argvtmp);
 		}

c7da8f3323dadab4e8e08a238452885f20aea322
Brian K Preston <brian8181@gmail.com>
2022-12-02 00:58:53 -0600

    debuging ...

--- src/rx.cpp
+++ src/rx.cpp
@@ -18,15 +18,15 @@ const int DEFAULT_ARGC = 2;
 const string VERSION_STRING = "rx 2.90";
 
 // options flags
-const unsigned short VERBOSE          = 0x01;
-const unsigned short IGNORE_CASE      = 0x02;
-const unsigned short SINGLE_MATCH     = 0x04;
-const unsigned short PRETTY_PRINT     = 0x08;
-const unsigned short GROUPS           = 0x10;
-const unsigned short EXTENDED_REGX    = 0x20;
-const unsigned short REGEX_OPTIONS    = 0x40;
+const unsigned short VERBOSE = 0x01;
+const unsigned short IGNORE_CASE = 0x02;
+const unsigned short SINGLE_MATCH = 0x04;
+const unsigned short PRETTY_PRINT = 0x08;
+const unsigned short GROUPS = 0x10;
+const unsigned short EXTENDED_REGX = 0x20;
+const unsigned short REGEX_OPTIONS = 0x40;
 const unsigned short SEARCH_FROM_FILE = 0x80;
-const unsigned short REGEX_FROM_FILE  = 0x100;
+const unsigned short REGEX_FROM_FILE = 0x100;
 const unsigned short DEFAULTS = PRETTY_PRINT | EXTENDED_REGX;
 
 // Set Defaults
@@ -34,35 +34,34 @@ unsigned short OPTION_FLAGS = DEFAULTS;
 regex::flag_type REGX_FLAGS = regex::ECMAScript;
 
 static struct option long_options[] =
-{
-	{"verbose", no_argument, 0, 'v'},
-	{"help", no_argument, 0, 'h'},
-	{"icase", no_argument, 0, 'i'},
-	{"single", no_argument, 0, 's'},
-	{"groups", no_argument, 0, 'g'},
-	{"pretty", no_argument, 0, 'P'},        //default
-	{"no-pretty", no_argument, 0, 'p'},
-	{"version", no_argument, 0, 'r'},
-	{"not_extended", no_argument, 0, 'e'}, 
-	{"extended", no_argument, 0, 'E'},      //default
-	{"options", required_argument, 0, 'o'}, //default
-	{"file", required_argument, 0, 'f'},
-	{"regex_file", required_argument, 0, 'x'}
-};
+	{
+		{"verbose", no_argument, 0, 'v'},
+		{"help", no_argument, 0, 'h'},
+		{"icase", no_argument, 0, 'i'},
+		{"single", no_argument, 0, 's'},
+		{"groups", no_argument, 0, 'g'},
+		{"pretty", no_argument, 0, 'P'}, // default
+		{"no-pretty", no_argument, 0, 'p'},
+		{"version", no_argument, 0, 'r'},
+		{"not_extended", no_argument, 0, 'e'},
+		{"extended", no_argument, 0, 'E'},		// default
+		{"options", required_argument, 0, 'o'}, // default
+		{"file", required_argument, 0, 'f'},
+		{"regex_file", required_argument, 0, 'x'}};
 
 map<std::string, regex::flag_type> regex_flags =
-{
-	{"ECMAScript", regex::ECMAScript}, 
-	{"basic", regex::basic},
-	{"extended", regex::extended},
-	{"awk", regex::awk},
-	{"grep", regex::grep}, 
-	{"egrep", regex::egrep},
-	{"icase", regex::icase},
-	{"nosubs", regex::nosubs},
-	{"optimize", regex::optimize},
-	{"collate", regex::collate},//
-	//{"multiline". regex::multiline} (since C++17)
+	{
+		{"ECMAScript", regex::ECMAScript},
+		{"basic", regex::basic},
+		{"extended", regex::extended},
+		{"awk", regex::awk},
+		{"grep", regex::grep},
+		{"egrep", regex::egrep},
+		{"icase", regex::icase},
+		{"nosubs", regex::nosubs},
+		{"optimize", regex::optimize},
+		{"collate", regex::collate}, //
+									 //{"multiline". regex::multiline} (since C++17)
 };
 
 void print_version()
@@ -72,19 +71,20 @@ void print_version()
 
 void print_help()
 {
-	cout << "Usage: "  
-		 << FMT_BOLD      << "rx"          << FMT_RESET << " "
+	cout << "Usage: "
+		 << FMT_BOLD << "rx" << FMT_RESET << " "
 		 << FMT_UNDERLINE << "[OPTION]..." << FMT_RESET << " "
-		 << FMT_UNDERLINE << "PATTERN"     << FMT_RESET << " "
-		 << FMT_UNDERLINE << "INPUT..."    << FMT_RESET << endl;
+		 << FMT_UNDERLINE << "PATTERN" << FMT_RESET << " "
+		 << FMT_UNDERLINE << "INPUT..." << FMT_RESET << endl;
 }
 
-void print_match_header(const string& pattern, const string& src, int count, int len)
+void print_match_header(const string &pattern, const string &src, int count, int len)
 {
-	if(OPTION_FLAGS & PRETTY_PRINT)
+	if (OPTION_FLAGS & PRETTY_PRINT)
 	{
-		if(len > 1) cout << count <<  ": "; // input number / count
-		cout << FMT_FG_RED << ((OPTION_FLAGS  & SINGLE_MATCH) ? "Single Match Pattern: " : "Match Pattern: ") << FMT_RESET
+		if (len > 1)
+			cout << count << ": "; // input number / count
+		cout << FMT_FG_RED << ((OPTION_FLAGS & SINGLE_MATCH) ? "Single Match Pattern: " : "Match Pattern: ") << FMT_RESET
 			 << "'" << FMT_FG_YELLOW << pattern << FMT_RESET << "'"
 			 << " -> "
 			 << FMT_FG_RED << "Input: " << FMT_RESET
@@ -92,34 +92,35 @@ void print_match_header(const string& pattern, const string& src, int count, int
 	}
 }
 
-int regx_match(const vector<string>& exp_text, const vector<string>& search_text)
+int regx_match(const vector<string> &exp_text, const vector<string> &search_text)
 {
 	int exp_text_len = exp_text.size();
 	// for each exp
-	for(int i = 0; i < exp_text_len; ++i)
+	for (int i = 0; i < exp_text_len; ++i)
 	{
 		int search_text_len = search_text.size();
 		// for each input
 		for (int j = 0; j < search_text_len; ++j)
 		{
-			print_match_header(exp_text[i], search_text[j], j+1, search_text_len);
+			print_match_header(exp_text[i], search_text[j], j + 1, search_text_len);
 			ostringstream oss;
 			ostringstream oss2;
+			ostringstream oss3;
 
-			REGX_FLAGS = (OPTION_FLAGS & IGNORE_CASE) != 0 ? REGX_FLAGS|regex::icase : REGX_FLAGS;
+			REGX_FLAGS = (OPTION_FLAGS & IGNORE_CASE) != 0 ? REGX_FLAGS | regex::icase : REGX_FLAGS;
 			regex src_epx;
 			try
 			{
 				src_epx = regex(exp_text[i], REGX_FLAGS);
 			}
-			catch(regex_error& e)
+			catch (regex_error &e)
 			{
 				cerr << "exception caught: " << e.what() << '\n';
 				cerr << "error of type " << e.code() << " was unhandled\n";
-			} 
+			}
 
 			auto begin = sregex_iterator(search_text[j].begin(), search_text[j].end(), src_epx, std::regex_constants::match_default);
-			auto end = sregex_iterator(); 
+			auto end = sregex_iterator();
 			int match_i = 0;
 			int prev_pos = 0;
 			int curr_pos = 0;
@@ -136,14 +137,14 @@ int regx_match(const vector<string>& exp_text, const vector<string>& search_text
 					break;
 				}
 
-				if(OPTION_FLAGS & PRETTY_PRINT)
+				if (OPTION_FLAGS & PRETTY_PRINT)
 				{
-					if(OPTION_FLAGS & GROUPS)
+					if (OPTION_FLAGS & GROUPS)
 					{
-						//Todo ...
+						// Todo ...
 						int len = match.size();
 						// BKP TESTING
-						//try 2
+						// try 2
 						short int spos1 = 0;
 						short int epos1 = 0;
 						short int spos2 = 0;
@@ -151,29 +152,50 @@ int regx_match(const vector<string>& exp_text, const vector<string>& search_text
 						stack<short int> pos_stack;
 
 						cout << endl;
-						for(int i = 0; i < len; ++i)
+
+						// testing
+						ptrdiff_t p_pos = 0;
+						ptrdiff_t c_pos = 0;
+						ptrdiff_t len_to_next = 0;
+						ptrdiff_t c_sz = distance(match[0].first, match[0].second);
+
+						for (int i = 1; i < len; ++i)
 						{
-							if(match[i].matched)
+							if (match[i].matched)
 							{
-								
-								//try 1
-								auto sm =  match[i];
-								auto sz1 = match.position(i);
-								auto pos1 = match.length(i);
-								string sss1 = search_text[j].substr(pos1, sz1);	
-								cout << "*DEBUG* 1* " << sss1 << " : " << endl;
+
+								auto sm = match[i];
+								// try 1
+								//  auto sz1 = match.position(i);
+								//  auto pos1 = match.length(i);
+								//  string sss1 = search_text[j].substr(pos1, sz1);
+								//  cout << "*DEBUG* 1* " << sss1 << " : " << endl;
 
 								// same thing as above ?
-								auto sz2 = distance(sm.first, sm.second);
-								auto pos2 = distance(match[0].first, sm.first);
-								string sss2 = search_text[j].substr(pos2, sz2);
-								oss2 << i << " " << sss2 << endl; 
-								cout << "*DEBUG* 2* " << sss2 << " : " << endl;		
-								
-									
-								cout << "*DEBUG* 3* " << sm.str() << " : " << endl;
+								c_sz = distance(sm.first, sm.second);
+								c_pos = distance(match[0].first, sm.first);
+
+								len_to_next = (c_pos - p_pos); // or end
+								string snip = search_text[j].substr(p_pos, len_to_next);
+								p_pos = c_pos;
+								oss3 << "(" << snip;
+
+								// get unmatched test before match
+								// oss3 << sss2;
+								// p_pos = c_pos + sz2;
+								// oss3 << "( " << CURRENT_FG_COLOR << sss2 << FMT_RESET;
+
+								// string sss2 = search_text[j].substr(pos2, sz2);
+								// oss2 << "(" << i << ":" << sss2 << ".";
+								// cout << "*DEBUG* 2* " << sss2 << " : " << endl;
+								// cout << "*DEBUG* 3* " << sm.str() << " : " << endl;
 							}
 						}
+
+						ptrdiff_t len_to_end = distance(match[0].first, match[0].second) - c_pos;
+						string snip_to_end = search_text[j].substr(c_pos, len_to_end);
+						oss3 << "(" << snip_to_end;
+
 						// BKP TESTING
 						cout << endl;
 					}
@@ -181,7 +203,7 @@ int regx_match(const vector<string>& exp_text, const vector<string>& search_text
 					{
 						curr_pos = match.position();
 						// get unmatched test before match
-						oss << search_text[j].substr(prev_pos, curr_pos-prev_pos);
+						oss << search_text[j].substr(prev_pos, curr_pos - prev_pos);
 						// set prev_position
 						prev_pos = curr_pos + match.length();
 						oss << "( " << CURRENT_FG_COLOR << match.str() << FMT_RESET << " )";
@@ -189,26 +211,28 @@ int regx_match(const vector<string>& exp_text, const vector<string>& search_text
 				}
 				else
 				{
-					cout << (match_i+1) << "\t" << search_text[j].substr(match.position(), match.length()) 
-						<< '\t' << match.position() << '\t' << match.length() << endl;
+					cout << (match_i + 1) << "\t" << search_text[j].substr(match.position(), match.length())
+						 << '\t' << match.position() << '\t' << match.length() << endl;
 				}
 			}
 
-			if(OPTION_FLAGS & PRETTY_PRINT)
+			if (OPTION_FLAGS & PRETTY_PRINT)
 			{
-				cout << endl << "Found " << distance(begin, end) << " matches:" << endl;
+				cout << endl
+					 << "Found " << distance(begin, end) << " matches:" << endl;
 				// get unmatched test before match
-				oss << search_text[j].substr(prev_pos, curr_pos-prev_pos);
+				oss << search_text[j].substr(prev_pos, curr_pos - prev_pos);
 				cout << oss.str() << endl;
-				//testing
+				// testing
 				cout << oss2.str() << endl;
+				cout << oss3.str() << endl;
 			}
 		}
 	}
 	return 0;
 }
 
-int parse_options(int argc, char* argv[])
+int parse_options(int argc, char *argv[])
 {
 	vector<string> exp_text;
 	vector<string> search_text;
@@ -254,16 +278,16 @@ int parse_options(int argc, char* argv[])
 		{
 			OPTION_FLAGS |= SEARCH_FROM_FILE;
 			ifstream search_file;
-			search_file.open(optarg, ios::in); 
+			search_file.open(optarg, ios::in);
 			if (search_file.is_open())
-			{   
+			{
 				string line;
 				search_text.clear();
-				while(getline(search_file, line))
+				while (getline(search_file, line))
 				{
 					search_text.push_back(line);
 				}
-				search_file.close(); 
+				search_file.close();
 			}
 			else
 			{
@@ -277,14 +301,14 @@ int parse_options(int argc, char* argv[])
 			OPTION_FLAGS |= REGEX_FROM_FILE;
 			ifstream exp_file;
 			exp_file.open(optarg, ios::in);
-			if(exp_file.is_open())
+			if (exp_file.is_open())
 			{
 				string line;
 				exp_file.clear();
-				while(getline(exp_file, line))
+				while (getline(exp_file, line))
 				{
 					exp_text.push_back(line);
-					//cout << line << endl;
+					// cout << line << endl;
 				}
 				exp_file.close();
 			}
@@ -301,19 +325,19 @@ int parse_options(int argc, char* argv[])
 			OPTION_FLAGS |= REGEX_OPTIONS;
 			string str_optarg = optarg;
 			string::size_type sz_beg = 0;
-			string::size_type sz_end = 0;	
-			
-			while(sz_end != string::npos) 
+			string::size_type sz_end = 0;
+
+			while (sz_end != string::npos)
 			{
-				sz_end = str_optarg.find('|', sz_beg);	
-				string split = str_optarg.substr(sz_beg, sz_end-sz_beg);
-				sz_beg = sz_end+1;
+				sz_end = str_optarg.find('|', sz_beg);
+				string split = str_optarg.substr(sz_beg, sz_end - sz_beg);
+				sz_beg = sz_end + 1;
 
 				try
 				{
 					REGX_FLAGS |= regex_flags.at(split);
 				}
-				catch(out_of_range const& ex)
+				catch (out_of_range const &ex)
 				{
 					cerr << ex.what() << endl;
 					cout << "Exception: Unexpected option, -h for help" << endl;
@@ -334,20 +358,20 @@ int parse_options(int argc, char* argv[])
 		return -1;
 	}
 
-	if(!((OPTION_FLAGS & SEARCH_FROM_FILE) && (OPTION_FLAGS & REGEX_FROM_FILE)))
-	{			
-		if(OPTION_FLAGS & SEARCH_FROM_FILE)
+	if (!((OPTION_FLAGS & SEARCH_FROM_FILE) && (OPTION_FLAGS & REGEX_FROM_FILE)))
+	{
+		if (OPTION_FLAGS & SEARCH_FROM_FILE)
 		{
-			exp_text.assign(argv+(optind), argv+(optind+1));
+			exp_text.assign(argv + (optind), argv + (optind + 1));
 		}
-		else if(OPTION_FLAGS & REGEX_FROM_FILE)
+		else if (OPTION_FLAGS & REGEX_FROM_FILE)
 		{
-			search_text.assign(argv+optind, argv + argc);
+			search_text.assign(argv + optind, argv + argc);
 		}
 		else
 		{
-			exp_text.assign(argv+optind, argv+(optind+1));
-			search_text.assign(argv+(optind+1), argv + argc);	
+			exp_text.assign(argv + optind, argv + (optind + 1));
+			search_text.assign(argv + (optind + 1), argv + argc);
 		}
 	}
 
